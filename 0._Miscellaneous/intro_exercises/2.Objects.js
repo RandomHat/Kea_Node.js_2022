553cc80b36368bf606da3fd924644bc603ab5d1c
Simon Roswall <simonroswall@gmail.com>
2022-08-29 13:16:10 +0200

    finished exercise 2

--- 0._Miscellaneous/intro_exercises/2.Objects.js
+++ 0._Miscellaneous/intro_exercises/2.Objects.js
@@ -9,11 +9,19 @@ const alienMessage = {
 
 // Log the message 
 
+console.log("Exercise 1:", alienMessage.message)
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const identity = {
+	name: "Simon",
+	age: 27
+}
+
+console.log("Exercise 2:", identity)
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -21,6 +29,10 @@ const alienMessage = {
 const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
+
+stackOverflow.isAllowed = true;
+
+console.log("Exercise 3:", stackOverflow.isAllowed)
 
 // --------------------------------------
 // Exercise 4 - Remove a property 
@@ -30,6 +42,13 @@ const thisSong = {
 }
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+delete thisSong.description;
+
+thisSong.about = "Just a tribute"
+thisSong.damn = "Couldn't remember the greatest song in the world, yeah - no!"
+
+console.log("Exercise 4:", thisSong )
 
 
 // --------------------------------------
