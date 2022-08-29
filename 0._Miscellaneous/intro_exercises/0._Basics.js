73e628e602d8dbd013c47ec7722804428090b431
Simon Roswall <simonroswall@gmail.com>
2022-08-29 10:34:17 +0200

    added notes about other solutions to 0._Basics.js

--- 0._Miscellaneous/intro_exercises/0._Basics.js
+++ 0._Miscellaneous/intro_exercises/0._Basics.js
@@ -10,11 +10,19 @@ const lastName = "Latif";
 // Print out the following in the console:
 // My first name is Anders and my last name is Latif
 
+// first solution
 const out = `My first name is ${firstName} and my last name is ${lastName}`
 
+// Wanted to remeber using comma in log.
 console.log("My first name is", firstName, "and my last name is", lastName)
 
 console.log(out)
+
+// Types of strings
+
+const messageOne = "I can write easily"
+const messageTwo = 'wow " does not need escaping';
+const messageThree = `""''""''""`
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -29,6 +37,15 @@ const number = 1;
 const sum = number + parseInt(year)
 
 console.log(sum)
+
+// Other solutions
+const two = Number(year) + number;
+const one = ~~year + number;
+const three = +year + number;
+
+// Differences
+console.log(parseInt("123124Sdfs34")) //Parses as much as possible
+console.log(Number("123124Sdfs34")) //Insists the string is a number (retuns the number NaN)
 
 // --------------------------------------
 
