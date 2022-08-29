09698ef9e0b64b95d495916c93f5e6b864e5327c
Simon Roswall <simonroswall@gmail.com>
2022-08-29 10:48:36 +0200

    Added homework

--- 0._Miscellaneous/intro.js
+++ 0._Miscellaneous/intro.js
@@ -8,3 +8,13 @@ console.log(sum);
 const balanceEquals = total === sum;
 
 console.log(balanceEquals)
+
+const cat ={
+    key:"value",
+    name:"Tom"
+}
+
+// square-bracket notation
+console.log(cat["name"])
+// dot notation
+console.log(cat.name)
