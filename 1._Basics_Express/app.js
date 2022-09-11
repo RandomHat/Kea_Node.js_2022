6807b1471ef0f9dcc26ebe995d214590a86df0d0
Simon Roswall <simonroswall@gmail.com>
2022-09-11 22:26:07 +0200

    added details from class about express getters and setup

--- 1._Basics_Express/app.js
+++ 1._Basics_Express/app.js
@@ -29,6 +29,48 @@ app.get("/cups", (req, res) => {
     })
 })
 
+
+// ways to send data with GET
+// works for all HTTP methods
+
+// 1.
+// path variables
+// /deers/{id] i spring
+// /deers/:id i express
+
+// 2.
+// query parameters (query string)
+// /deers?key=value&key2=value2
+
+// with path variables
+
+app.get ("/deers/:id", (req, res) => {
+    //console.log(req.params)
+    if (Number(req.params.id) === 1){
+        res.send({
+            name: "Bambi",
+            theBestAndOGDeer: true
+        })
+    } else {
+        res.send({
+            error: "Resource not found",
+            id : 404
+        })
+    }
+    
+})
+
+// with query string
+
+app.get("/actors/", (req,res) =>{
+    console.log(req.query.name)
+    res.send({
+        message: "Information about actor:",
+        ...req.query
+    })
+}) 
+
+
 // Set port, and start server. Best practise er at have den her i bunden. Det kan også give problemer at have app.listen() længere oppe i appen.
 app.listen(8080, () => {
     console.log("Server is running on port", 8080)
